283ed13b10b51b4d0be8d4ef2deeb99b730df6e8
lovedeep singh kamal <developer.lovedeep@gmail.com>
2022-09-03 23:51:48 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Memory game
+
+a simple memory game using HTML,CSS, JS
+![screenshot](./images/ss.png)
+
+## How to run locally
+just clone the repository or download the files and open `index.html`
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,67 @@
+
+const cards = document.querySelectorAll('.memory-card')
+const restartBtn = document.querySelector('#restart-btn')
+const timer = document.querySelector('#timer')
+let counter = 0;
+
+//increasing the counter
+const interval = setInterval(function(){
+    counter++;
+    console.log()
+    timer.innerHTML = "<b>" + counter + "</b>";
+  }, 1000);
+
+function restartGame(){
+    window.location.reload()
+}
+
+const flippedCards = []
+let matched=0;
+
+function checkForMatch(){
+    //if matched
+    if(flippedCards[0].dataset.character===flippedCards[1].dataset.character)
+    {
+        //remove listener from the card to avoid clicking again
+        flippedCards[0].removeEventListener('click',flipCard)
+        flippedCards[1].removeEventListener('click',flipCard)
+        
+        matched++;
+        if(matched===6)
+        {
+        alert("hurrah! you did it")
+        clearInterval(interval)
+        }
+        else
+        alert("woah! matched")
+        
+    }
+    //if not matched
+    else
+    {
+        flippedCards.forEach(flippedCard=>{
+            flippedCard.children[1].style.display="block";
+        })
+        alert("haha! better luck next time");
+    }
+
+    flippedCards.length= 0;
+}
+
+function flipCard(e){
+    const card = e.target
+    flippedCards.push(card)
+    card.children[1].style.display="none";
+
+    //when we have filled two cards check for the match
+    if(flippedCards.length === 2)
+    {
+        setTimeout(checkForMatch,0); 
+    }
+}
+
+cards.forEach(card=>{
+    card.addEventListener('click',flipCard);
+})
+
+
