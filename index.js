b6d466d08f35e5b23b1ecc728b8e1cadd4fa692b
DNA5769 <dennisthomas2002@gmail.com>
2022-10-01 20:56:13 +0530

    Storing image positions in JS and correspondingly updated game logic

--- index.js
+++ index.js
@@ -15,12 +15,27 @@ function restartGame(){
     window.location.reload()
 }
 
+// Storing image sources for list of cards
+const cardImageSrcs = [
+    'images/cards/inosuke.png',
+    'images/cards/nezuko.png',
+    'images/cards/rengoku.png',
+    'images/cards/mask.png',
+    'images/cards/tanjiro.png',
+    'images/cards/zenitsu.png',
+    'images/cards/inosuke.png',
+    'images/cards/nezuko.png',
+    'images/cards/rengoku.png',
+    'images/cards/mask.png',
+    'images/cards/tanjiro.png',
+    'images/cards/zenitsu.png',
+];
 const flippedCards = []
 let matched=0;
 
 function checkForMatch(){
     //if matched
-    if(flippedCards[0].dataset.character===flippedCards[1].dataset.character)
+    if(flippedCards[0].children[0].src===flippedCards[1].children[0].src) // Checking if the flipped cards have same src i.e are matching
     {
         //remove listener from the card to avoid clicking again
         flippedCards[0].removeEventListener('click',flipCard)
@@ -40,6 +55,7 @@ function checkForMatch(){
     else
     {
         flippedCards.forEach(flippedCard=>{
+            flippedCard.children[0].src="#"; // Removing image src so that it isn't visible through HTML
             flippedCard.children[1].style.display="block";
         })
         alert("haha! better luck next time");
@@ -48,20 +64,21 @@ function checkForMatch(){
     flippedCards.length= 0;
 }
 
-function flipCard(e){
+function flipCard(e, i){
     const card = e.target
     flippedCards.push(card)
+    card.children[0].src=cardImageSrcs[i]; // Setting image source for flipped front face
     card.children[1].style.display="none";
 
     //when we have filled two cards check for the match
     if(flippedCards.length === 2)
     {
-        setTimeout(checkForMatch,0); 
+        setTimeout(checkForMatch,100); // Adding a small delay, so that card gets enough time to render updated src before alert pops up
     }
 }
 
-cards.forEach(card=>{
-    card.addEventListener('click',flipCard);
+cards.forEach((card, i)=>{
+    card.addEventListener('click',e=>flipCard(e, i)); // Passing index of card when calling flipCard
 })
 
 
