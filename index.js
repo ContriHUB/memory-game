2f9baf6232ca67f89bb7e5e56a10f82a4dc3a09b
Harsh Narayan <narayanharsh2000@gmail.com>
2022-10-05 22:05:13 +0530

    added new levels

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ function toggleSound(){
 
 // Wrapping entire code in anonymous function and calling it, so that user doesn't have access to cardImageSrcs
 (() => {
+    const game = document.querySelector('.memory-game');
+  function openM(){
+    alert("man");
+  }
+    let level = window.prompt("ask me?", "1/2/3");
+    const noOfCards = level == 1 ? 12 : (level == 2 ? 20 : 24) ;
+    console.log(noOfCards);
+    for(let i = 0; i < noOfCards; i++) {
+        var newCard = document.createElement("div");
+        newCard.classList.add("memory-card");
+        var img1 = document.createElement("img");
+        img1.classList.add("front-face");
+        img1.src = "#";
+        img1.alt = "card front face";
+        var img2 = document.createElement("img");
+        img2.classList.add("back-face");
+        img2.src = "images/cover.png";
+        img2.alt = "card cover";
+
+        newCard.appendChild(img1);
+        newCard.appendChild(img2);
+
+        document.querySelector('.memory-game').appendChild(newCard); 
+    }
     const cards = document.querySelectorAll('.memory-card');
     const timer = document.querySelector('#timer')
     const bestTimer = document.querySelector('#best--timer');
@@ -42,24 +66,44 @@ function toggleSound(){
     // Storing image sources for list of cards
     // Storing it as a list and not a matrix to make it a bit difficult to map list to the 3x4 grid
     const cardImageSrcs = [
-        'images/cards/inosuke.png',
-        'images/cards/nezuko.png',
+        //need upto 12 cards for level 1
         'images/cards/rengoku.png',
-        'images/cards/mask.png',
-        'images/cards/tanjiro.png',
+        'images/cards/rengoku.png',
+        'images/cards/zenitsu.png',
         'images/cards/zenitsu.png',
         'images/cards/inosuke.png',
+        'images/cards/inosuke.png',
         'images/cards/nezuko.png',
-        'images/cards/rengoku.png',
+        'images/cards/nezuko.png',
+        'images/cards/mask.png',
         'images/cards/mask.png',
         'images/cards/tanjiro.png',
-        'images/cards/zenitsu.png',
+        'images/cards/tanjiro.png',
+        //need upto 20 cards for level 2
+        'images/cards/inosuke2.png',
+        'images/cards/inosuke2.png',
+        'images/cards/kanao.png',
+        'images/cards/kanao.png',
+        'images/cards/kimetsu.png',
+        'images/cards/kimetsu.png',
+        'images/cards/nezuko2.png',
+        'images/cards/nezuko2.png',
+        //need upto 24 cards for level 3
+        'images/cards/tokito.png',
+        'images/cards/tokito.png',
+        'images/cards/genya.png',
+        'images/cards/genya.png',
     ];
+
+    //slice the array of image source up to the number of cards needed
+    const cardArray = cardImageSrcs.slice(0,noOfCards);
+    // console.log(cardArray)
+
     const flippedCards = []
     let matched=0;
 
     function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
+        let currentIndex = noOfCards,  randomIndex;
     
         // While there remain elements to shuffle.
         while (currentIndex != 0) {
@@ -75,14 +119,14 @@ function toggleSound(){
         return array;
     }
     // Shuffling cards
-    shuffle(cardImageSrcs);
+    shuffle(cardArray);
 
     function checkForMatch(){
         //if matched
         if(flippedCards[0].children[0].src===flippedCards[1].children[0].src) // Checking if the flipped cards have same src i.e are matching
         {        
             matched++;
-            if(matched===6)
+            if(matched === noOfCards / 2)
             {
                 
                 if(audioState){
@@ -136,8 +180,8 @@ function toggleSound(){
             return;
 
         flippedCards.push(card)
-        card.children[0].src=cardImageSrcs[i]; // Setting image source for flipped front face
-        card.children[0].alt=cardImageSrcs[i].split('/').slice(-1)[0].split('.').slice(0, -1).join('.'); // Setting image file name as alt text for flipped front face
+        card.children[0].src=cardArray[i]; // Setting image source for flipped front face
+        card.children[0].alt=cardArray[i].split('/').slice(-1)[0].split('.').slice(0, -1).join('.'); // Setting image file name as alt text for flipped front face
         card.children[1].style.display="none";
 
         //when we have filled two cards check for the match
